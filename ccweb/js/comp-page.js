3f7327320c98748c707a973da0ba779205e87687
Saransh Grover <ycubiksrube@gmail.com>
2017-05-18 17:10:58 -0400

    Update comp-page.js

--- ccweb/js/comp-page.js
+++ ccweb/js/comp-page.js
@@ -76,3 +76,13 @@ window.onscroll = function(e) {
  */
 offSetManager();
 });
+
+function toggleColumn(n) {
+    var currentClass = document.getElementById("mytable").className;
+    if (currentClass.indexOf("show"+n) != -1) {
+        document.getElementById("mytable").className = currentClass.replace("show"+n, "");
+    }
+    else {
+        document.getElementById("mytable").className += " " + "show"+n;
+    }
+}
